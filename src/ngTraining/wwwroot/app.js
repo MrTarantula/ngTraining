e450cc6dd548ec0290870a51ee40b446e721031a
ryantauriainen_test <ryan.tauriainen@oscn.net>
2015-07-21 09:19:38 -0500

    Added files from session 1

--- src/ngTraining/wwwroot/app.js
+++ src/ngTraining/wwwroot/app.js
@@ -1,3 +1,5 @@
 ﻿(function () {
-    
+    'use strict';
+
+    angular.module('app', []);
 })(); 
--- src/ngTraining/wwwroot/party.controller.js
+++ src/ngTraining/wwwroot/party.controller.js
@@ -1,3 +1,25 @@
 ﻿(function () {
-    
+    'use strict';
+
+    angular.module('app').controller('PartyController', PartyController);
+
+    PartyController.$inject = ['$scope'];
+
+    function PartyController($scope) {
+
+        var vm = this;
+
+        vm.crimes = ["Murder", "Armed Robery", "Assault"];
+        vm.parties = [];
+
+        vm.add = function() {
+            vm.parties.push($scope.newParty);
+        }
+
+        vm.clear = function() {
+            $scope.newParty = null;
+        }
+    };
+
+
 })();
